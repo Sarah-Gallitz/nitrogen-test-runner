91f454ac7bdfe60a9cc6924ad22b18cce45567ff
Sarah Gallitz <sarah.gallitz.devs@gmail.com>
2023-02-26 14:00:14 +1100

    Add an empty hello world android app

---
+++ demo/app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "au.sgallitz.nitrogen.demo"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "au.sgallitz.nitrogen.demo"
+
+        minSdk = 21
+        targetSdk = 33
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.2"
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
+
+    implementation("androidx.activity:activity-compose:1.6.1")
+
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
---
+++ demo/app/src/main/java/au/sgallitz/nitrogen/demo/MainActivity.kt
@@ -0,0 +1,21 @@
+package au.sgallitz.nitrogen.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+        }
+    }
+
+    @Composable
+    private fun HomeScreen() {
+        Text(text = "Hello World")
+    }
+}
