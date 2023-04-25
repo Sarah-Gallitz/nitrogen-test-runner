2ea28718881ad9d4ea5a5cfbe2f29a1c3ff8762e
Sarah Gallitz <sarah.gallitz.devs@gmail.com>
2023-04-25 13:14:28 +1000

    Refactor how the config file is loaded

--- nitrogen-plugin/src/main/kotlin/au/sgallitz/nitrogen/plugin/NitrogenTestConfiguration.kt
+++ nitrogen-plugin/src/main/kotlin/au/sgallitz/nitrogen/plugin/NitrogenTestConfiguration.kt
@@ -2,12 +2,22 @@ package au.sgallitz.nitrogen.plugin
 
 import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Project
+import java.io.FileNotFoundException
 
 class NitrogenTestConfiguration(private val project: Project) {
     private val config: Map<String, String>
+    private val configFileName = "nitrogen.config"
 
     init {
-        config = project.file("nitrogenTest.config").readText().split("\n")
+        val configFile = if (project.file(configFileName).exists()) {
+            project.file(configFileName)
+        } else if (project.rootProject.file(configFileName).exists()) {
+            project.rootProject.file(configFileName)
+        } else {
+            throw FileNotFoundException("Nitrogen plugin requires a $configFileName file")
+        }
+
+        config = configFile.readText().split("\n")
             .filter { it.contains("=") }
             .associate { it.split("=")[0].trim() to it.split("=")[1].trim() }
     }
@@ -17,11 +27,11 @@ class NitrogenTestConfiguration(private val project: Project) {
 
     val targetApp: Project
         get() = if (targetProjectPath.isBlank()) {
-            throw IllegalArgumentException("targetProjectPath must be configured")
+            throw IllegalArgumentException("Nitrogen $configFileName must configure the targetProjectPath")
         } else {
             val targetProject = project.rootProject.findProject(targetProjectPath)
             if (targetProject == null || !targetProject.plugins.hasPlugin("com.android.application")) {
-                throw IllegalArgumentException("targetProjectPath must be an android application module")
+                throw IllegalArgumentException("Nitrogen $configFileName targetProjectPath must be of type android application module")
             } else {
                 targetProject
             }
